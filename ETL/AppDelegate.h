8aed0fa5d23579b0a01689e2da6042be0687001d
Maciej Komorowski <mckomo@gmail.com>
2013-05-14 08:07:26 +0200

    Initial Commit

---
+++ ETL/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  ETL
+//
+//  Created by Maciej Komorowski on 14.05.2013.
+//  Copyright (c) 2013 Maciej Komorowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ ETL/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ETL
+//
+//  Created by Maciej Komorowski on 14.05.2013.
+//  Copyright (c) 2013 Maciej Komorowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
