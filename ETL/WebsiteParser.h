6d6e6e27e935154415a8f77a01909f7204cb6ac3
cojoj <matthewhare91@gmail.com>
2013-05-15 21:18:25 +0200

    Added files for WebsiteParser class

---
+++ ETL/WebsiteParser.h
@@ -0,0 +1,13 @@
+//
+//  WebsiteParser.h
+//  ETL
+//
+//  Created by Mateusz Zajac on 15.05.2013.
+//  Copyright (c) 2013 Maciej Komorowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebsiteParser : NSObject
+
+@end
---
+++ ETL/WebsiteParser.m
@@ -0,0 +1,13 @@
+//
+//  WebsiteParser.m
+//  ETL
+//
+//  Created by Mateusz Zajac on 15.05.2013.
+//  Copyright (c) 2013 Maciej Komorowski. All rights reserved.
+//
+
+#import "WebsiteParser.h"
+
+@implementation WebsiteParser
+
+@end
