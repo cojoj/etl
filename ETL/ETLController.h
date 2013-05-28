d975249236a3ac1761a8ab90788778fdb23abf8c
Maciej Komorowski <mckomo@gmail.com>
2013-05-28 13:29:50 +0200

    Semantic fixed

--- ETL/ETLController.h
+++ ETL/ETLController.h
@@ -29,7 +29,7 @@ typedef enum {
 
 -(id) init;
 -(void) downloadWebsitesContent;
--(void) extractCompanyData;
+-(void) extractCompaniesData;
 -(void) saveExtracedData;
 -(void) fullCycle;
 -(void) restart;
